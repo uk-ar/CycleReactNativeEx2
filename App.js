72b950c741388ac8f84d38cb558463ce902d7486
uk-ar <yuuki.ari@gmail.com>
2017-09-29 05:16:51 +0900

    Add MultiSelectList

--- App.js
+++ App.js
@@ -9,6 +9,61 @@ import {
   CycleRoot,
 } from './cycle-react-native';
 
+class MyListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.id);
+  };
+
+  render() {
+    const textColor = this.props.selected ? "red" : "black";
+    //this._onPress avoid changeing props in TouchableOpacity
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View>
+          <Text style={{ color: textColor }}>
+            {this.props.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+class MultiSelectList extends React.PureComponent {
+  state = {selected: (new Map(): Map<string, boolean>)};
+
+  _keyExtractor = (item, index) => item.id;
+
+  _onPressItem = (id: string) => {
+    // updater functions are preferred for transactional updates
+    this.setState((state) => {
+      // copy the map rather than modifying state.
+      const selected = new Map(state.selected);
+      selected.set(id, !selected.get(id)); // toggle
+      return {selected};
+    });
+  };
+
+  _renderItem = ({item}) => (
+    <MyListItem
+      id={item.id}
+      onPressItem={this._onPressItem}
+      selected={!!this.state.selected.get(item.id)}
+      title={item.title}
+    />
+  );
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.data}
+        extraData={this.state}
+        keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+      />
+    );
+  }
+}
 function main({RN}) {
   return {
     RN: RN.select('button').events('press')
@@ -17,19 +72,10 @@ function main({RN}) {
           .startWith(0)
           .scan((x,y) => x+y)
           .map(i =>
-            <FlatList
-            data={[{key: 'a'}, {key: 'b'},{key: 'c'}]}
-              renderItem={({item}) =>
-                <Cycle.TouchableOpacity selector="button" payload={item}>
-                  <View>
-                    <Text style={{  }}>
-                                {//this.props.title
-                                 //onPress={()=>console.log("press")}
-                      }
-                      {item.key}
-                    </Text>
-                  </View>
-                </Cycle.TouchableOpacity>}
+            <MultiSelectList
+              data={[{id: 'a',title:"foo"},
+                     {id: 'b',title:"bar"},
+                     {id: 'c',title:"baz"}]}
             />
           ),
   };
