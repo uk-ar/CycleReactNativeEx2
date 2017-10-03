bd9a08c7829f201e81047c21aafdfd5a3609dfb0
uk-ar <yuuki.ari@gmail.com>
2017-10-04 00:15:46 +0900

    Optimize icon

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -48,6 +48,22 @@ storiesOf('Book', module)
       }}
     />
   ))
+  .add('with short title', () => (
+    <BookCell
+      onPress={action('clicked-bookcell')}
+      book={{
+        title: 'guri & gura', author: 'author baz',
+        thumbnail: 'http://thumbnail.image.rakuten.co.jp/@0_mall/book/cabinet/2147/9784834032147.jpg?_ex=200x200',
+        author: 'author foo',
+        bucket: 'liked',
+        isbn: '9784834032147',
+        active: true,
+      }}
+    >
+      {/* <LibraryStatus text="bar" style={{color:"blue"}} loading={true}/> */}
+      <LibraryStatus text="bar" style={{color:"blue"}} loading={false}/>
+    </BookCell>
+  ))
   .add('with library status', () => (
     <BookCell
       onPress={action('clicked-bookcell')}
