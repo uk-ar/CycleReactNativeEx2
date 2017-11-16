adca099d3839c87aa29f08a58b1528a0432eede5
uk-ar <yuuki.ari@gmail.com>
2017-11-17 06:36:23 +0900

    Replace main

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import Expo from 'expo'
 
 import { SearchScene } from './components/SearchScene';
 
+//const intent = require('./intent');
+//const model = require('./model');
+//const view = require('./view');
 
 import {
   Cycle,
@@ -15,70 +18,16 @@ import {
   CycleRoot,
 } from './cycle-react-native';
 
-class MyListItem extends React.PureComponent {
-  _onPress = () => {
-    this.props.onPressItem(this.props.id);
-  };
-
-  render() {
-    const textColor = this.props.selected ? "red" : "black";
-    //this._onPress avoid changeing props in TouchableOpacity
-    return (
-      <TouchableOpacity onPress={this._onPress}>
-        <View>
-          <Text style={{ color: textColor }}>
-            {this.props.title}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
-
-class MultiSelectList extends React.PureComponent {
-  state = {selected: (new Map(): Map<string, boolean>)};
-
-  _keyExtractor = (item, index) => item.id;
-
-  _onPressItem = (id: string) => {
-    // updater functions are preferred for transactional updates
-    this.setState((state) => {
-      // copy the map rather than modifying state.
-      const selected = new Map(state.selected);
-      selected.set(id, !selected.get(id)); // toggle
-      return {selected};
-    });
-  };
-
-  _renderItem = ({item}) => (
-    <MyListItem
-      id={item.id}
-      onPressItem={this._onPressItem}
-      selected={!!this.state.selected.get(item.id)}
-      title={item.title}
-    />
-  );
-
-  render() {
-    return (
-      <FlatList
-        data={this.props.data}
-        extraData={this.state}
-        keyExtractor={this._keyExtractor}
-        renderItem={this._renderItem}
-      />
-    );
-  }
-}
 function main({RN, HTTP}) {
+  //const actions = intent(RN, HTTP);
+  //const state$ = model(actions);
   return {
-    RN: RN.select('button').events('press')
+    RN: RN.select('search').events('press')
           .do(args => console.log('foo0:', args))
-          .map(ev => +1)
           .startWith(0)
-          .scan((x,y) => x+y)
           .map(i =>
             <SearchScene
+              selector={"search"}
               showLoadingIcon={true}
               selectedIndex={1}
               rejects={[]}
