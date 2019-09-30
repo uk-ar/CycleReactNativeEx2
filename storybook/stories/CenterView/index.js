6bebedee647e940971e83dc772d60a427eecda68
uk-ar <yuuki.ari@gmail.com>
2019-10-01 06:40:28 +0900

    Work with Basic example

--- storybook/stories/CenterView/index.js
+++ storybook/stories/CenterView/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
-import style from './style';
+import { View, StyleSheet } from 'react-native';
 
-const CenterView = props => <View style={style.main}>{props.children}</View>;
+const CenterView =
+  props =>
+    <View style={styles.main}>
+      {props.children}
+    </View>;
+
+const CenterLeftView =
+  props =>
+    <View style={styles.left}>
+      {props.children}
+    </View>;
 
 CenterView.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export { CenterView as default };
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  left: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    backgroundColor: '#F5FCFF',
+  }
+})
+module.exports = { CenterView,CenterLeftView };
