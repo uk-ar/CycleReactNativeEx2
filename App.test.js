6abc5d3f335f960ff0215e5b7b74371ea924bbc5
uk-ar <yuuki.ari@gmail.com>
2019-10-04 06:03:09 +0900

    Add test

--- App.test.js
+++ App.test.js
@@ -2,8 +2,11 @@ import React from 'react';
 import App from './App';
 
 import renderer from 'react-test-renderer';
+import initStoryshots from '@storybook/addon-storyshots';
 
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
 });
+
+initStoryshots();
