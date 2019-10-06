fa831252d700d5f61454a0c9dfc33735f23da28b
uk-ar <yuuki.ari@gmail.com>
2019-10-06 20:40:12 +0900

    Add Closeable

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -185,7 +185,39 @@ class FadeInView extends React.Component {
   }
 }
 
-storiesOf('Closable', module)
+class CloseableView extends React.Component {
+  constructor(props) {
+    super(props);
+    this._animated = new Animated.Value(0);
+  }
+
+  render() {
+    let { closed, style } = this.props
+    Animated.timing(
+      this._animated,{
+        toValue: closed ? 0 : 1,
+        duration: 250,
+      }
+    ).start();
+    return (
+      <Animated.View
+        style={[{
+            opacity: this._animated,
+            height: this._animated.interpolate({
+              inputRange:[0,1],
+              outputRange:[0,50],
+              extrapolate:'clamp'
+            }),
+            overflow:"hidden"
+          },style]}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
+storiesOf('Closeable', module)
   //.addDecorator(getStory => <CenterLeftView>{getStory(10)}</CenterLeftView>)
   .add('open0', () => (
     <Counter>
@@ -210,4 +242,24 @@ storiesOf('Closable', module)
          )
        }}
     </Counter>
+  ))
+  .add('close', () => (
+    <Counter>
+      {(i)=>{
+         return (
+           <View>
+             <CloseableView closed={ i % 2 ? true : false }>
+               <View style={{
+                 height:50, backgroundColor:"red", //flex:1
+               }}/>
+             </CloseableView>
+           </View>
+         )
+       }}
+    </Counter>
+  ))
+  .add('close2', () => (
+    <View style={{
+      height:50, backgroundColor:"red", //flex:1
+    }}/>
   ))
